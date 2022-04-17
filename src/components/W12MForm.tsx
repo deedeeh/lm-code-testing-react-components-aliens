5462ff03d6b6003fe72e5e734608917f11ac09cf
deedeeh <dinahafez.8@gmail.com>
2022-04-17 22:30:37 +0100

    Added form element and a button - consoled log form fields on submit

--- src/components/W12MForm.tsx
+++ src/components/W12MForm.tsx
@@ -1,4 +1,4 @@
-import { useState, ChangeEvent } from 'react';
+import { useState, ChangeEvent, FormEvent } from 'react';
 import W12MHeader from './W12MHeader';
 import SpeciesName from './SpeciesName';
 import PlanetName from './PlanetName';
@@ -13,29 +13,41 @@ const W12MForm = () => {
 	const [ whatIs2Plus2, setWhaIs2Plus2 ] = useState<string>('Not 4');
 	const [ reasonForSparing, setReasonForSparing ] = useState<string>('');
 
+	const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		console.log(`Species Name: ${speciesName}`);
+		console.log(`Planet Name: ${planetName}`); 
+		console.log(`Number of beings: ${numberOfBeings}`);
+		console.log(`What is 2 + 2? ${whatIs2Plus2}`);
+		console.log(`Reason for sparing: ${reasonForSparing}`);
+	}
+
 	return (
 		<section className='w12MForm'>
-			<W12MHeader />
-			<SpeciesName 
-				speciesName={speciesName}
-				onChangeSpeciesName={(e: ChangeEvent<HTMLInputElement>) => setSpeciesName(e.target.value)}
-			/>
-			<PlanetName 
-				planetName={planetName}
-				onChangePlanetName={(e: ChangeEvent<HTMLInputElement>) => setPlanetName(e.target.value)}
-			/>
-			<NumberOfBeings 
-				numberOfBeings={numberOfBeings}
-				onChangeNumberOfBeings={(e: ChangeEvent<HTMLInputElement>) => setNumberOfBeings(e.target.value)}
-			/>
-			<WhatIs2Plus2 
-				whatIs2Plus2={whatIs2Plus2}
-				onChangeWhatIs2Plus2={(e: ChangeEvent<HTMLSelectElement>) => setWhaIs2Plus2(e.target.value)}
-			/>
-			<ReasonForSparing 
-				reasonForSparing={reasonForSparing}
-				onChangeReasonForSparing={(e: ChangeEvent<HTMLTextAreaElement>) => setReasonForSparing(e.target.value)}
-			/>
+			<form onSubmit={handleSubmit}>
+				<W12MHeader />
+				<SpeciesName 
+					speciesName={speciesName}
+					onChangeSpeciesName={(e: ChangeEvent<HTMLInputElement>) => setSpeciesName(e.target.value)}
+				/>
+				<PlanetName 
+					planetName={planetName}
+					onChangePlanetName={(e: ChangeEvent<HTMLInputElement>) => setPlanetName(e.target.value)}
+				/>
+				<NumberOfBeings 
+					numberOfBeings={numberOfBeings}
+					onChangeNumberOfBeings={(e: ChangeEvent<HTMLInputElement>) => setNumberOfBeings(e.target.value)}
+				/>
+				<WhatIs2Plus2 
+					whatIs2Plus2={whatIs2Plus2}
+					onChangeWhatIs2Plus2={(e: ChangeEvent<HTMLSelectElement>) => setWhaIs2Plus2(e.target.value)}
+				/>
+				<ReasonForSparing 
+					reasonForSparing={reasonForSparing}
+					onChangeReasonForSparing={(e: ChangeEvent<HTMLTextAreaElement>) => setReasonForSparing(e.target.value)}
+				/>
+				<button type='submit'>Submit</button>
+			</form>
 		</section>
 	);
 };
