e297ac728edb8f5eb67eeabe77ba10c11a7d784f
deedeeh <dinahafez.8@gmail.com>
2022-04-20 22:37:26 +0100

    Added a test to check for calling onChange with the correct params

--- src/components/SpeciesName.test.tsx
+++ src/components/SpeciesName.test.tsx
@@ -1,24 +1,36 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import SpeciesName from './SpeciesName';
 import userEvent from '@testing-library/user-event';
 
-describe('speciesName component', () => {
+const mockedOnChangeSpeciesName = jest.fn();
+
+describe('<SpeciesName />', () => {
   const speciesNameData = {
     speciesName: 'Humans',
-    onChangeSpeciesName: () => {}
+    onChangeSpeciesName: mockedOnChangeSpeciesName,
   }
   
-  test('renders speciesName label element', () => {
-    render(<SpeciesName {...speciesNameData} />)
+  test('renders speciesName component', () => {
+    render(<SpeciesName {...speciesNameData} />);
     const speciesNameLabelText = screen.getByLabelText(/^Species Name:$/i);
     expect(speciesNameLabelText).toBeInTheDocument();
   });
 
-  test('displays speciesName passed prop value', () => {  
+  test('displays speciesName passed prop value when typing', () => {  
     render(<SpeciesName {...speciesNameData} />);
     const speciesNameElement = screen.getByRole('textbox');
     userEvent.type(speciesNameElement, speciesNameElement.value);
-    expect(speciesNameElement).toHaveValue('Humans');
+    expect(speciesNameElement).toHaveValue(speciesNameElement.value);
+  });
+
+  test('calls onChange function with passed onChangeSpeciesName prop', () => {
+    render(<SpeciesName {...speciesNameData} />);
+    const speciesNameElement = screen.getByRole('textbox');
+    const onChangeProp = speciesNameData.onChangeSpeciesName;
+    speciesNameElement.onChange = onChangeProp;
+    fireEvent.change(speciesNameElement, { target: { value: 'Aliens' } });
+    expect(onChangeProp).toHaveBeenCalled();
+    expect(onChangeProp).toHaveBeenCalledTimes(1);
   });
 });
 
