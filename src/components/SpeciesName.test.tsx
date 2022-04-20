6fa91fcfa710441140a8204cda2c96a66b44e0c0
deedeeh <dinahafez.8@gmail.com>
2022-04-20 19:38:54 +0100

    Changed testing input value with userEvent.type

--- src/components/SpeciesName.test.tsx
+++ src/components/SpeciesName.test.tsx
@@ -1,23 +1,26 @@
 import { render, screen } from '@testing-library/react';
 import SpeciesName from './SpeciesName';
+import userEvent from '@testing-library/user-event';
 
 describe('speciesName component', () => {
-  const spciesNameData = {
+  const speciesNameData = {
     speciesName: 'Humans',
     onChangeSpeciesName: () => {}
   }
+  
   test('renders speciesName label element', () => {
-    render(<SpeciesName {...spciesNameData} />)
+    render(<SpeciesName {...speciesNameData} />)
     const speciesNameLabelText = screen.getByLabelText(/^Species Name:$/i);
     expect(speciesNameLabelText).toBeInTheDocument();
   });
 
-  test('displays speciesName passed prop value', () => {
-    render(<SpeciesName {...spciesNameData} />)
-    const speciesNameInputValue = screen.getByDisplayValue(/Humans/i);
-    expect(speciesNameInputValue).toBeInTheDocument();
+  test('displays speciesName passed prop value', () => {  
+    render(<SpeciesName {...speciesNameData} />);
+    const speciesNameElement = screen.getByRole('textbox');
+    userEvent.type(speciesNameElement, speciesNameElement.value);
+    expect(speciesNameElement).toHaveValue('Humans');
   });
-})
+});
 
 
 
