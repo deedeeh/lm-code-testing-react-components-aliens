d2b7c36b2db50932e72003a95346e30abfffda1f
deedeeh <dinahafez.8@gmail.com>
2022-04-20 18:50:56 +0100

    Added a test to check input value will be displayed if passed as prop

--- src/components/SpeciesName.test.tsx
+++ src/components/SpeciesName.test.tsx
@@ -1,12 +1,23 @@
 import { render, screen } from '@testing-library/react';
 import SpeciesName from './SpeciesName';
 
-test('renders speciesName label element', () => {
+describe('speciesName component', () => {
   const spciesNameData = {
     speciesName: 'Humans',
     onChangeSpeciesName: () => {}
   }
-  render(<SpeciesName {...spciesNameData} />)
-  const speciesNameLabelText = screen.getByLabelText(/^Species Name:$/i);
-  expect(speciesNameLabelText).toBeInTheDocument();
-});
+  test('renders speciesName label element', () => {
+    render(<SpeciesName {...spciesNameData} />)
+    const speciesNameLabelText = screen.getByLabelText(/^Species Name:$/i);
+    expect(speciesNameLabelText).toBeInTheDocument();
+  });
+
+  test('displays speciesName passed prop value', () => {
+    render(<SpeciesName {...spciesNameData} />)
+    const speciesNameInputValue = screen.getByDisplayValue(/Humans/i);
+    expect(speciesNameInputValue).toBeInTheDocument();
+  });
+})
+
+
+
