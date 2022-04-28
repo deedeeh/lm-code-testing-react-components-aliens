f377ac6c024b79791e00ff630d93e0b77f464304
deedeeh <dinahafez.8@gmail.com>
2022-04-28 23:13:46 +0100

    Added validation to ReasonForSparing component input

--- src/components/ReasonForSparing.tsx
+++ src/components/ReasonForSparing.tsx
@@ -1,17 +1,36 @@
-import { ChangeEvent } from 'react';
+import { ChangeEvent, useState } from 'react';
+import ErrorMessage from './ErrorMessage';
 
 interface ReasonForSparingProps {
   reasonForSparing: string;
   onChangeReasonForSparing: (e: ChangeEvent<HTMLTextAreaElement>) => void;
 }
 
-const ReasonForSparing: React.FC<ReasonForSparingProps> = ({ reasonForSparing, onChangeReasonForSparing }) => 
-  <div>
-    <label htmlFor='reasonForSparing'>
-      Reason for sparing:
-      &nbsp;<textarea id='reasonForSparing' value={reasonForSparing} onChange={onChangeReasonForSparing}>
-      </textarea>
-    </label>
-  </div>
+const ReasonForSparing: React.FC<ReasonForSparingProps> = ({ reasonForSparing, onChangeReasonForSparing }) => {
+  const [ errorMessage, setErrorMessage ] = useState<string | undefined>('');
+
+  const validate: (input: string) => string | undefined = input => 
+    input.length < 17 || input.length > 153 ? 'ERROR - Reason for sparing must be between 17 and 153 characters.' : undefined;
+  
+
+  return (
+    <div>
+      <label htmlFor='reasonForSparing'>
+        Reason for sparing:
+        &nbsp;
+        <textarea 
+          id='reasonForSparing' 
+          value={reasonForSparing} 
+          onChange={(e) => {
+            const errorMessage = validate(e.target.value);
+            setErrorMessage(errorMessage);
+            onChangeReasonForSparing(e)
+          }}>
+        </textarea>
+      </label>
+      <ErrorMessage message={errorMessage} />
+    </div>
+  )
+}
 
 export default ReasonForSparing;
